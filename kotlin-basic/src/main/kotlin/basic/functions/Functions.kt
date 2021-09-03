b3d4f23a2970bef08a2dab0f105eb06178ca3773
pinest94 <doingnow94@gmail.com>
2021-09-03 18:45:27 +0900

    [210903] Add extension function demo

--- kotlin-basic/src/main/kotlin/basic/functions/Functions.kt
+++ kotlin-basic/src/main/kotlin/basic/functions/Functions.kt
@@ -1,11 +1,25 @@
-package basic
+package basic.functions
 
 
 fun maxOfTwoNumbers(a: Int, b: Int): Int = if (a > b) a else b
 
 fun main() {
-    println(maxOfTwoNumbers(10, 20))
-    println(printName("hansol"))
+    val view: View = Button()
+    view.click()
+    view.showoff()
 }
 
 fun printName(name: String) = "Hello, $name"
+
+fun String.lastChar(): Char = get(length-1)
+
+open class View {
+    open fun click() = println("View Clicked")
+}
+
+open class Button: View() {
+    override fun click() = println("Button Clicked")
+}
+
+fun View.showoff() = println("*** view ***")
+fun Button.showoff() = println("*** button ***")
