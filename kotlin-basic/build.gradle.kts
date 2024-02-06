8672ce573a04ee24d0fc16c69fa3017fade89c6d
pinest94 <doingnow94@gmail.com>
2024-02-06 19:17:48 +0900

    Fix code

--- kotlin-basic/build.gradle.kts
+++ kotlin-basic/build.gradle.kts
@@ -1,21 +1,55 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.32"
+    kotlin("jvm") version "1.9.22"
+    id("com.google.protobuf") version "0.9.4"
 }
 
 group = "me.user"
 version = "1.0-SNAPSHOT"
 
+val protobufVersion = "3.25.2"
+val grpcVersion = "1.61.0"
+val grpcKotlinVersion = "1.4.0"
+
 repositories {
     mavenCentral()
+    google()
 }
 
 dependencies {
     implementation("org.junit.jupiter:junit-jupiter:5.7.0")
+    implementation("com.google.protobuf:protobuf-kotlin:$protobufVersion")
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:$protobufVersion"
+    }
+
+    plugins {
+        create("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion"
+        }
+        create("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:$grpcKotlinVersion:jdk8@jar"
+        }
+    }
+
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                create("grpc")
+                create("grpckt")
+            }
+            it.builtins {
+                create("kotlin")
+            }
+        }
+    }
 }
 
 tasks.test {
@@ -23,5 +57,5 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "11"
 }
